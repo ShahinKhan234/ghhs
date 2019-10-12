d03668e51f9893a865abf2496c2d4b1b0b4afc63
ShahinKhan234 <freelancershahin34@gmail.com>
2019-10-13 00:46:10 +0600

    first commit

---
+++ clock/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Clock</title>
+	<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="clock.css">
+</head>
+<body>
+
+<div id="clock">
+	<ul id="clockFace">
+		<li id="sec"></li>
+		<li id="min"></li>
+		<li id="hour"></li>
+
+	</ul>
+</div>
+
+
+<script type="text/javascript">
+	$(document).ready(function() {
+         
+              setInterval( function() {
+              var seconds = new Date().getSeconds();
+              var sdegree = seconds * 6;
+              var srotate = "rotate(" + sdegree + "deg)";
+              
+              $("#sec").css({"-moz-transform" : srotate, "-webkit-transform" : srotate});
+                  
+              }, 1000 );
+              
+         
+              setInterval( function() {
+              var hours = new Date().getHours();
+              var mins = new Date().getMinutes();
+              var hdegree = hours * 30 + (mins / 2);
+              var hrotate = "rotate(" + hdegree + "deg)";
+              
+              $("#hour").css({"-moz-transform" : hrotate, "-webkit-transform" : hrotate});
+                  
+              }, 1000 );
+        
+   setInterval( function() {
+              var mins = new Date().getMinutes();
+              var mdegree = mins * 6;
+              var mrotate = "rotate(" + mdegree + "deg)";
+              
+              $("#min").css({"-moz-transform" : mrotate, "-webkit-transform" : mrotate});
+                  
+              }, 1000 );
+         
+        });
+</script>
+</body>
+</html>
---
+++ test/js/nav.js
@@ -0,0 +1,20 @@
+
+$(window).on('scroll',function(){
+			if ($(window).scrollTop()) {
+				$("nav").addClass("black");
+			}
+			else{
+				$("nav").removeClass("black");
+			}
+		})
+$(document).ready(function(){
+	$(".menu h4").click(function(){
+		$("nav ul").toggleClass("active");
+	})
+
+})
+
+/*slider*/
+ $('#myCarousel').carousel({
+    interval: 3000,
+ })
